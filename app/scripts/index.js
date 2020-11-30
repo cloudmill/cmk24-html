9f6c1bfa86427806fad5ec9ca9b232c490e98e80
Dmitry68 <iMac@iMac-iMac.netis>
2020-11-30 12:25:18 +0300

    [UPDATE] add header

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,4 +3,20 @@ import App from "./main.js";
 
 $(document).ready(function() {
   let app = new App();
+  headerScroll();
 });
+
+let headerScroll = function() {
+  $(window).on('scroll', function(){
+    $('header').addClass('header--scroll');
+    $('.header__logo').addClass('header__logo--scroll');
+    $('.header__phone').addClass('header__phone--scroll');
+    $('.header__button').addClass('header__button--scroll');
+    if( $(window).scrollTop() == 0 ){
+      $('header').removeClass('header--scroll');
+      $('.header__logo').removeClass('header__logo--scroll');
+      $('.header__phone').removeClass('header__phone--scroll');
+      $('.header__button').removeClass('header__button--scroll');
+    }
+  });
+}
